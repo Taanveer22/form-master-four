cc0444a8b8a7934e6dc9ed4971ba98e996749406
Taanveer22 <taanveer469@gmail.com>
2025-10-07 20:32:10 +0600

    alhamdulliah simple form submitted by onSubmit event handler via e.target.name.value

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SimpleForm from "./components/SimpleForm";
+
+function App() {
+  return (
+    <div className="w-5/6 mx-auto my-10">
+      <SimpleForm></SimpleForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SimpleForm.jsx
@@ -0,0 +1,29 @@
+const SimpleForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit btn clicked...............");
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log(e.target.password.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="space-y-3">
+        <h1>Simple Form</h1>
+        <input type="text" name="name" className="border-2 border-gray-800" />
+        <br />
+        <input type="email" name="email" className="border-2 border-gray-800" />
+        <br />
+        <input
+          type="password"
+          name="password"
+          className="border-2 border-gray-800"
+        />
+        <br /> <br />
+        <input type="submit" value="Submit Now" className="bg-green-600 p-1" />
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
